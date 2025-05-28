08324a5471d53830e7fd3ee6d226a3e39b565761
tnhs <sontnh@twinger.vn>
2025-05-28 22:37:36 +0700

    add dracula theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -6,23 +6,23 @@ return {
     config = function()
       require("catppuccin").setup({
         flavour = "mocha", -- latte, frappe, macchiato, mocha
-        background = { -- :h background
+        background = {     -- :h background
           light = "latte",
           dark = "mocha",
         },
         transparent_background = true, -- disables setting the background color.
-        show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-        term_colors = false,       -- sets terminal colors (e.g. `g:terminal_color_0`)
+        show_end_of_buffer = false,    -- shows the '~' characters after the end of buffers
+        term_colors = false,           -- sets terminal colors (e.g. `g:terminal_color_0`)
         dim_inactive = {
-          enabled = false,         -- dims the background color of inactive window
+          enabled = false,             -- dims the background color of inactive window
           shade = "dark",
-          percentage = 0.15,       -- percentage of the shade to apply to the inactive window
+          percentage = 0.15,           -- percentage of the shade to apply to the inactive window
         },
-        no_italic = false,         -- Force no italic
-        no_bold = false,           -- Force no bold
-        no_underline = false,      -- Force no underline
-        styles = {                 -- Handles the styles of general hi groups (see `:h highlight-args`):
-          comments = { "italic" }, -- Change the style of comments
+        no_italic = false,             -- Force no italic
+        no_bold = false,               -- Force no bold
+        no_underline = false,          -- Force no underline
+        styles = {                     -- Handles the styles of general hi groups (see `:h highlight-args`):
+          comments = { "italic" },     -- Change the style of comments
           conditionals = {},
           loops = {},
           functions = {},
@@ -53,7 +53,17 @@ return {
         },
       })
       -- setup must be called before loading
-      vim.cmd.colorscheme("catppuccin")
+      -- vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+  {
+    "Mofiqul/dracula.nvim",
+    config = function()
+      require("dracula").setup({
+        transparent_bg = true, -- enable transparent background
+        italic_comment = true, -- enable italic comments
+      })
+      vim.cmd.colorscheme("dracula")
     end,
   },
 }
