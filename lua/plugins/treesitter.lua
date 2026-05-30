2b345a487a3c475422bdf9aa1533988386e9abbb
REATAN S6 <nghiatrangbuonkhivangbongem@gmail.com>
2026-05-30 08:00:20 +0000

    update

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,48 +1,49 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	branch = "main",
+  "nvim-treesitter/nvim-treesitter",
+  branch = "main",
   build = "TSUpdate",
-	version = false, -- last release is way too old and doesn't work on Windows
-	-- event = "VeryLazy",
-event = { "BufReadPost", "BufNewFile" },
-	-- cmd = { "TSUpdate", "TSInstall", "TSLog", "TSUninstall" },
-	opts = {
-		-- LazyVim config for treesitter
-		ensure_installed = {
-			"bash",
-			"c",
-			"diff",
-			"html",
-			"javascript",
-			"jsdoc",
-			"json",
-			"jsonc",
-			"lua",
-			"luadoc",
-			"luap",
-			"markdown",
-			"markdown_inline",
-			"printf",
-			"python",
-			"query",
-			"regex",
-			"toml",
-			"typescript",
-			"vim",
-			"vimdoc",
-			"xml",
-			"yaml",
-			"php",
-			"scss",
-		},
-		-- indent = { enable = true },
-		highlight = { enable = true,
-    additional_vim_regex_highlighting = false,
+  version = false, -- last release is way too old and doesn't work on Windows
+  -- event = "VeryLazy",
+  event = { "BufReadPost", "BufNewFile" },
+  cmd = { "TSUpdate", "TSInstall", "TSLog", "TSUninstall" },
+  opts = {
+    -- LazyVim config for treesitter
+    ensure_installed = {
+      "bash",
+      "c",
+      "diff",
+      "html",
+      "javascript",
+      "jsdoc",
+      "json",
+      "jsonc",
+      "lua",
+      "luadoc",
+      "luap",
+      "markdown",
+      "markdown_inline",
+      "printf",
+      "python",
+      "query",
+      "regex",
+      "toml",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "xml",
+      "yaml",
+      "php",
+      "scss",
+    },
+    -- indent = { enable = true },
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+    },
   },
-	},
-	config = function(_, opts)
-		local TS = require("nvim-treesitter")
-		-- setup treesitter
-		TS.setup(opts)
-	end,
+  config = function(_, opts)
+    local TS = require("nvim-treesitter")
+    -- setup treesitter
+    TS.setup(opts)
+  end,
 }
