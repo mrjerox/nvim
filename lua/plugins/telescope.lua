04f37dffc83f3253a78dacde9fe3f47dcc031ddd
TNHS <nghiatrangbuonkhivangbongem@gmail.com>
2026-07-24 15:48:27 +0700

    update nvim-tree and clean

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,30 +1,61 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	version = "*",
-	dependencies = {
-		{ "nvim-lua/plenary.nvim" },
-		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-	},
-	opts = {
-		defaults = {
-			file_ignore_patterns = {
-				"node_modules",
-				".git",
-				".vscode",
+	{
+		"nvim-tree/nvim-tree.lua",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		opts = {
+			sort = {
+				sorter = "case_sensitive",
+			},
+			view = {
+				width = 30,
+			},
+			renderer = {
+				group_empty = true,
+			},
+			filters = {
+				dotfiles = true,
+			},
+			update_focused_file = {
+				enable = true,
 			},
 		},
-		pickers = {
-			find_files = {
-				hidden = true,
+		config = function(_, opts)
+			require("nvim-tree").setup(opts)
+			vim.keymap.set("n", "<C-b>", function()
+				vim.cmd("NvimTreeToggle")
+			end, { desc = "Telescope find files", noremap = true })
+		end,
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		version = "*",
+		dependencies = {
+			{ "nvim-lua/plenary.nvim" },
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		},
+		opts = {
+			defaults = {
+				file_ignore_patterns = {
+					"node_modules",
+					".git",
+					".vscode",
+				},
+			},
+			pickers = {
+				find_files = {
+					hidden = true,
+				},
 			},
 		},
+		config = function(_, opts)
+			require("telescope").setup(opts)
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+		end,
 	},
-	config = function(_, opts)
-		require("telescope").setup(opts)
-		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
-	end,
 }
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,5 +1,52 @@
 return {
 	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		version = false, -- last release is way too old and doesn't work on Windows
+		-- event = { "BufReadPost", "BufNewFile" },
+		build = ":TSUpdate",
+		event = "VeryLazy",
+		cmd = { "TSUpdate", "TSInstall", "TSLog", "TSUninstall" },
+		opts = {
+			ensure_installed = {
+				"bash",
+				"c",
+				"css",
+				"diff",
+				"html",
+				"javascript",
+				"jsdoc",
+				"json",
+				"jsonc",
+				"lua",
+				"luadoc",
+				"luap",
+				"markdown",
+				"markdown_inline",
+				"printf",
+				"python",
+				"query",
+				"regex",
+				"toml",
+				"typescript",
+				"vim",
+				"vimdoc",
+				"xml",
+				"yaml",
+				"php",
+				"scss",
+			},
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+		},
+		config = function(_, opts)
+			local TS = require("nvim-treesitter.config")
+			TS.setup(opts)
+		end,
+	},
+	{
 		"Mofiqul/dracula.nvim",
 		opts = {
 			transparent_bg = true, -- enable transparent background
